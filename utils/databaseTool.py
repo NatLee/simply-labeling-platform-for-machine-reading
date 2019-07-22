1116718c91a3855c2bc995c82c3cae19d601ae79
Nat Lee <natlee@NatLee-Macbook.local>
2019-07-22 17:50:04 +0800

    Update database.

--- utils/databaseTool.py
+++ utils/databaseTool.py
@@ -1,59 +1,112 @@
 import sqlite3
 import datetime
+import random
 
 class MrcDatabase(object):
-    '''
-    MRC SQLite Toolkit
-    '''
+    """MRC Database SQLite toolkit.
+
+    Attributes:
+        sqlitePath (str): The sqlite file path.
+
+    """
     def __init__(self, sqlitePath:str):
 
+        self.__timezone = datetime.timezone(datetime.timedelta(hours=8))
         self.__conn = sqlite3.connect(sqlitePath)
-        
+        self.__maxArticleNumber = self.__getMaxArticleNumber()
+        self.__maxQuestionAndAnswerNumber = self.__getMaxQuestionAndAnswerNumber()
 
     def insertArticle(self, article:str):
         query = 'INSERT into article VALUES (NULL, ?, ?)'
-        timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
-        with self.__conn.cursor() as cursor:
+        timestamp = datetime.datetime.now().replace(tzinfo=self.__timezone).isoformat()
+        with self.__conn:
+            cursor = self.__conn.cursor()
             cursor.execute(query, (article, timestamp))
 
-    def insertQuestionAnswer(self, question:str, answerStart:str, answerString:str):
-        query = 'INSERT into question_answer VALUES (NULL, ?, ?, ?)'
-        timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
-        with self.__conn.cursor() as cursor:
-            cursor.execute(query, (question, answerStart, answerString, timestamp))
+    def insertQuestionAnswer(self, articleId:int, question:str, answerStart:int, answerString:str):
+        query = 'INSERT into question_answer VALUES (NULL, ?, ?, ?, ?, ?)'
+        timestamp = datetime.datetime.now().replace(tzinfo=self.__timezone).isoformat()
+        with self.__conn:
+            cursor = self.__conn.cursor()
+            cursor.execute(query, (articleId, question, answerStart, answerString, timestamp))
 
     def commit(self):
         self.__conn.commit()
 
-    ##
-    def getMaxArticleNumber(self):
+    def __getMaxArticleNumber(self):
         query = 'SELECT count(*) FROM article'
-        with self.__conn.cursor() as cursor:
-            result = cursor.execute(query)
+        with self.__conn:
+            cursor = self.__conn.cursor()
+            cursor.execute(query)
+            result = cursor.fetchone()[0]
+        return result
+
+    def __getMaxQuestionAndAnswerNumber(self):
+        query = 'SELECT count(*) FROM question_answer'
+        with self.__conn:
+            cursor = self.__conn.cursor()
+            cursor.execute(query)
+            result = cursor.fetchone()[0]
         return result
 
 
-    def getArticleById(self, idx:int):
-        query = 'SELECT context FROM article WHERE idx = ?'
-        with self.__conn.cursor() as cursor:
-            result = cursor.execute(query,(idx,))
+    def getRandomArticle(self) -> str:
+        idx = random.randint(1, self.__maxArticleNumber)
+        query = 'SELECT context FROM article WHERE id = ?'
+        with self.__conn:
+            cursor = self.__conn.cursor()
+            cursor.execute(query,(idx,))
+            result = cursor.fetchone()[0]
+        return result
+
+
+    def getArticleById(self, idx:int) -> str:
+        if idx > self.__maxArticleNumber:
+            result = 'ERROR: <ArticleOutOfBound>'
+        else:
+            query = 'SELECT context FROM article WHERE id = ?'
+            with self.__conn:
+                cursor = self.__conn.cursor()
+                cursor.execute(query,(idx,))
+                result = cursor.fetchone()[0]
         return result
 
     def getAllArticle(self):
         query = 'SELECT context FROM article'
-        with self.__conn.cursor() as cursor:
-            result = cursor.execute(query)
+        with self.__conn:
+            cursor = self.__conn.cursor()
+            cursor.execute(query)
+            result = cursor.fetchall()
         return result
 
 
-    def getQuestionAndAnswerById(self, idx:int):
-        query = 'SELECT question, answer_start, answer_string FROM article WHERE idx = ?'
-        with self.__conn.cursor() as cursor:
-            result = cursor.execute(query, (idx,))
+    def getQuestionAndAnswerByArticleId(self, articleId:int) -> (str, int, str):
+        if articleId > self.__maxArticleNumber:
+            result = ('NULL', -1, 'NULL')
+        else:    
+            query = 'SELECT question, answer_start, answer_string FROM question_answer WHERE article_id = ?'
+            with self.__conn:
+                cursor = self.__conn.cursor()
+                cursor.execute(query, (articleId,))
+                result = cursor.fetchone()
+        return result
+
+
+    def getQuestionAndAnswerById(self, idx:int) -> (str, int, str):
+        if idx > self.__maxQuestionAndAnswerNumber:
+            result = ('NULL', -1, 'NULL')
+        else:    
+            query = 'SELECT question, answer_start, answer_string FROM question_answer WHERE id = ?'
+            with self.__conn:
+                cursor = self.__conn.cursor()
+                cursor.execute(query, (idx,))
+                result = cursor.fetchone()[0]
         return result
 
     def getAllQuestionAndAnswer(self):
-        query = 'SELECT question, answer_start, answer_string FROM article'
-        with self.__conn.cursor() as cursor:
-            result = cursor.execute(query)
+        query = 'SELECT question, answer_start, answer_string FROM question_answer'
+        with self.__conn:
+            cursor = self.__conn.cursor()
+            cursor.execute(query)
+            result = cursor.fetchall()
         return result
