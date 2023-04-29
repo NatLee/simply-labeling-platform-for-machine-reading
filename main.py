9f769a94ee67dbd7a56d538ea1b504b44a9edc12
NatLee <natlee.work@gmail.com>
2023-04-29 15:06:39 +0800

    [update] fix route

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ from flask import Flask, render_template, request, jsonify
 from utils import MrcDatabase
 
 app = Flask(__name__)
-mcd = MrcDatabase('mrc.label')
+db = MrcDatabase('mrc.label')
 
+@app.route('/article', methods=['GET'])
+def get_article():
+    articleId, article = db.getRandomArticle()
+    return jsonify({'article_id': articleId, 'article': article, 'timestamp': datetime.datetime.now().replace(tzinfo=datetime.timezone(datetime.timedelta(hours=8))).isoformat()})
 
-@app.route('/getArticle', methods=['POST'])
-def getArticle():
-    articleId, article = mcd.getRandomArticle()
-    return jsonify({'article_id': articleId, 'article':article, 'timestamp': datetime.datetime.now().replace(tzinfo=datetime.timezone(datetime.timedelta(hours=8))).isoformat()})
+@app.route("/article", methods=["POST"])
+def insert_article():
+    article = request.form.get("article")
+    description = request.form.get("description")
 
-@app.route('/insertQuestionAnswer', methods=['POST'])
-def insertQuestionAnswer():
+    if article: # only check field of article
+        db.insertArticle(article, description)
+        return {"status": "success", "message": "Article inserted successfully."}, 200
+
+    return {"status": "error", "message": "Article or description is missing."}, 400
+
+@app.route('/question-answer', methods=['POST'])
+def insert_question_and_answer():
     questionAnswers = request.get_json()
     for questionAnswer in questionAnswers:
         articleId = questionAnswer.get('article_id')
@@ -23,7 +33,7 @@ def insertQuestionAnswer():
         if articleId is  None or question is None or ansStart is None:
             continue
         else:
-            mcd.insertQuestionAnswer(articleId, question, ansStart, ansString)
+            db.insertQuestionAnswer(articleId, question, ansStart, ansString)
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
@@ -34,4 +44,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8787, debug=True)
-    
